e6aed2634def84e6d233ba25c7767a363f81639e
Ankit <ankit@Ankits-MacBook-Pro.local>
2025-12-06 00:39:12 -0800

    al

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'all things blog',
+  title: 'All Things Blogs',
   description: 'all things blog',
 }
 
